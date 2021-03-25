5d7e47cbaa272887bc45b01e6fcf1c9f6846f77c
Maxwell Meyer <maxreadswell@gmail.com>
2021-03-25 16:36:42 -0700

    Make some final adjustments to html and stylesheet.
    Co-authored-by: Adrian Camacho <adriancamacho@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
     const side2 = $("#dim2").val();
     const side3 = $("#dim3").val();
     let result;
-       if (side1 === side2 && side2 === side3 && side1 === side3) {
-         result = "equilateral";
-       } else if (side1 === side2 || side1 === side3) {
-         result = "isosceles";
-       } else if (side1 !== side2 || side2 !== side3) {
-         result = "scalene";
-       } else {
-         alert("That is not a triangle... SORRY! enter a number...");
-       }
+      if (side1 === side2 && side2 === side3 && side1 === side3) {
+        result = "equilateral";
+      } else if (side1 === side2 || side1 === side3) {
+        result = "isosceles";
+      } else if (side1 !== side2 || side2 !== side3) {
+        result = "scalene";
+      } else {
+        alert("That is not a triangle... SORRY! enter a number...");
+      }
     $("#result").text(result);
     $("#theDIMS").show();     
   });
