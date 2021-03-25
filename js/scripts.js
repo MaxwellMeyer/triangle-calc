3bcda6cd56faf53a8f577b2bfa0f6d0246beacac
Maxwell Meyer <maxreadswell@gmail.com>
2021-03-25 16:18:19 -0700

    Add working JS functionality to page.
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#triangle").submit(function(event) {
+    event.preventDefault();
+    const side1 = $("#dim1").val();
+    const side2 = $("#dim2").val();
+    const side3 = $("#dim3").val();
+    let result;
+       if (side1 === side2 && side2 === side3 && side1 === side3) {
+         result = "equilateral";
+       } else if (side1 === side2 || side1 === side3) {
+         result = "isosceles";
+       } else if (side1 !== side2 || side2 !== side3) {
+         result = "scalene";
+       } else {
+         alert("That is not a triangle... SORRY! enter a number...");
+       }
+    $("#result").text(result);
+    $("#theDIMS").show();     
+  });
+});
